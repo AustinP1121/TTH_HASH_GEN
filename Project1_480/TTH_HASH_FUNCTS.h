3574caa9b5f456300ec0b0ccf64349c90151b65f
Austin P <apedigo@moreheadstate.edu>
2023-07-26 20:24:23 -0400

    Added functionality to handle the formation of each block

--- Project1_480/TTH_HASH_FUNCTS.h
+++ Project1_480/TTH_HASH_FUNCTS.h
@@ -20,12 +20,14 @@
 using namespace std;
 
 int LetterToNumber(char);
+vector<vector<char>> CreateBlocks(string, int*);
 
 //method to convert the letter to a number
 int LetterToNumber(char letter)
 {
 	int letterIndex = -1;
 
+	//making a string from the single char
 	string letterString(1, letter);
 
 	//lookup dictionary for each letter
@@ -54,4 +56,71 @@ int LetterToNumber(char letter)
 	/* will return -1 if the piped letter
 	is not a member of the alphabet */
 	return letterIndex;
+}
+
+vector<vector<char>> CreateBlocks(string message, int * blocks)
+{
+	double numberOfBlocks;
+	int index = 0;
+	vector<vector<char>> blockCollection;
+	vector<char> block;
+	
+	/*remove anything that isn't 
+	an alphabetic char from the message*/
+	for (int i = 0; i < message.length(); i++)
+	{
+		if (message[i] < 'a' || message[i] < 'A' 
+			|| message[i] > 'z' || message[i] > 'Z')
+		{
+			message.erase(i, 1); 
+			i--;
+		}
+	}
+
+	//even count of blocks w/o need for nulls
+	if (message.size() % 8 == 0)
+	{
+		//compute the number of blocks to encrypt
+		numberOfBlocks = message.length() / 16;
+
+		for (int i = 0; i < numberOfBlocks; i++)
+		{
+			for (int j = 0; j < 16; j++)
+			{
+				block.push_back(message[index]);
+				index++;
+			}
+			blockCollection.push_back(block);
+			block.clear();
+		}
+	}
+	//not an even count of blocks, need to pad out the last block with nulls
+	else
+	{
+		//compute the number of blocks to encrypt
+		numberOfBlocks = message.length() / 16;
+
+		numberOfBlocks = round(numberOfBlocks);
+
+		for (int i = 0; i < numberOfBlocks; i++)
+		{
+			for (int j = 0; j < 16; j++)
+			{
+				if (index > message.size())
+				{
+					block.push_back(NULL);
+				}
+				else
+				{
+					block.push_back(message[index]);
+
+				}
+				index++;
+			}
+			blockCollection.push_back(block);
+			block.clear();
+		}
+	}
+
+	return blockCollection;
 }
